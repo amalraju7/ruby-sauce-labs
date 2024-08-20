c1a9d4ab47611f3bde1d188fd84b10f7474f3944
amalraju <amalraju443@gmail.com>
2024-08-20 14:52:36 -0400

    driver issues

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ source 'https://rubygems.org'
 gem 'cucumber'
 gem 'rspec'
 gem 'report_builder'
-# Gemfile
-gem 'selenium-webdriver', '~> 4.0'
+
+gem 'selenium-webdriver'
 gem 'test-unit'
 gem 'bundle'
--- features/support/hook.rb
+++ features/support/hook.rb
@@ -1,16 +1,32 @@
+# Before hook - Initialize browser
 Before do
-    $browser = Selenium::WebDriver::Chrome .for :chrome
+  $browser = Selenium::WebDriver.for :chrome
 end
 
+# After hook - Handle scenario failures and take screenshots
 After do |scenario|
   if scenario.failed?
-    screenshot = "screenshots/#{scenario.name}.png"
-    $browser.save_screenshot(screenshot)
-    embed(screenshot, 'image/png', 'Screenshot')
+    # Ensure the directory exists before saving the screenshot
+    screenshot_dir = "screenshots"
+    Dir.mkdir(screenshot_dir) unless Dir.exist?(screenshot_dir)
+
+    # Generate a valid filename
+    screenshot_file = "#{screenshot_dir}/#{sanitize_filename(scenario.name)}.png"
+
+    # Save the screenshot
+    $browser.save_screenshot(screenshot_file)
   end
+
+  # Quit the browser
   $browser.quit
 end
 
+# At exit - Build the report
 at_exit do
   ReportBuilder.build_report
+end
+
+# Utility method to sanitize filenames (e.g., replace invalid characters)
+def sanitize_filename(filename)
+  filename.gsub(/[\/\:\*\?"<>\|]/, '_')
 end
